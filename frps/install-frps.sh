6161d2234af8401b68283d5daa8403016244e1b8
glorygods <67311131+glorygods@users.noreply.github.com>
2023-07-08 19:42:25 +0800

    Update install-frps.sh

--- frps/install-frps.sh
+++ frps/install-frps.sh
@@ -13,7 +13,7 @@ program_init="/etc/init.d/${program_name}"
 program_config_file="frps.ini"
 ver_file="/tmp/.frp_ver.sh"
 program_version_link="https://raw.githubusercontent.com/glorygods/onekey-install-shell/master/frps/version.sh"
-str_install_shell=https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/install-frps.sh
+str_install_shell=https://raw.githubusercontent.com/glorygods/onekey-install-shell/master/frps/install-frps.sh
 shell_update(){
     fun_clangcn "clear"
     echo "Check updates for shell..."
